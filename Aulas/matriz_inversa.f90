07e749050a3b5356b2389d4547278e4677947549
Henrique-zoo <morcelleshenrique@gmail.com>
2025-09-24 10:50:00 -0300

    correcao

--- Aulas/matriz_inversa.f90
+++ Aulas/matriz_inversa.f90
@@ -2,10 +2,10 @@ program matriz_inversa
     implicit none
     integer :: i, j, k, n
     double precision, dimension(:), allocatable :: x, b, c
-    double precision, dimension(:,:), allocatable :: M, M_inv, L, U, Id
+    double precision, dimension(:,:), allocatable :: M, M_inv, L, U, Id, prod
 
     n = 5
-    allocate(L(n, n), U(n, n), M(n, n), M_inv(n, n), Id(n, n), x(n), b(n), c(n))
+    allocate(L(n, n), U(n, n), M(n, n), M_inv(n, n), prod(n, n), Id(n, n), x(n), b(n), c(n))
 
     M = reshape([2.d0, 1.d0, -1.d0, 1.d0, 3.d0, &
         1.d0, 0.d0, 2.d0, -1.d0, 1.d0, &
@@ -26,15 +26,18 @@ program matriz_inversa
     end do
 
     print *, "Matriz inversa M_inv:"
-    do i = 1, n
-        print *, M_inv(i, :)
-    end do
+    call exibir_matriz(M_inv)
 
-    deallocate(L, U, M, M_inv, x, c)
+    call prod_matrizes(M, M_inv, prod)
+    print *, "Produto M x M_inv"
+    call exibir_matriz(prod)
+
+    deallocate(L, U, M, M_inv, x, c, prod)
 contains
     subroutine fatoracao_LU(A, L, U)
         double precision, dimension(:,:), intent(in) :: A
         double precision, dimension(:,:), intent(out) :: L, U
+        integer :: i, j
         
         U = A
         L = 0.0d0
@@ -56,6 +59,7 @@ contains
         double precision, dimension(:), intent(in) ::  b
         double precision, dimension(:), intent(out) ::  x
         double precision :: soma
+        integer :: i, j
 
         x(n) = b(n) / U(n, n)
         do i = n-1, 1, -1
@@ -72,14 +76,41 @@ contains
         double precision, dimension(:), intent(in) :: b
         double precision, dimension(:), intent(out) :: c
         double precision :: soma
+        integer :: i, j
         
         c(1) = b(1)
         do i = 2, size(c)
             soma = 0
-            do j = 1, i
+            do j = 1, i-1
                 soma = soma + L(i, j)*c(j)
             end do
             c(i) = b(i) - soma
         end do
     end subroutine subs_direta
+
+    subroutine prod_matrizes(A,  B, C)
+        double precision, dimension(:,:), intent(in) :: A, B
+        double precision, dimension(:,:), intent(out) :: C
+        do i = 1, n
+            do j = 1, n
+                C(i, j) = 0.0d0
+                do k = 1, n
+                    C(i, j) = C(i, j) + A(i, k) * B(k, j)
+                end do
+            end do
+        end do
+    end subroutine prod_matrizes
+
+    subroutine exibir_matriz(A)
+        double precision, dimension(:,:), intent(in) :: A
+        integer :: i, j, n
+
+        n = size(A, 1)
+        do i = 1, n
+            do j = 1, n
+                write(*, '(F10.4)', advance='no') A(i, j)
+            end do
+            print *
+        end do
+    end subroutine exibir_matriz
 end program matriz_inversa
